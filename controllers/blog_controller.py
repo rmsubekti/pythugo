f77534318be0b0cb2ea68ce5f72e71207f0f3fc0
Rahmat Subekti <rahmatsubekti@live.com>
2024-06-24 12:25:59 +0700

    use recursive tree & get assets from cdn

--- controllers/blog_controller.py
+++ controllers/blog_controller.py
@@ -60,15 +60,42 @@ def editor(blog_id):
     owner=db.session.execute(select(Author).where(Author.id==blog.owner_id)).first()
     tree = github.get('repos/'+blog.repo, token=owner[0].github_token.to_token())
     repo = tree.json()
-    return render_template("blog/editor.html", blog=blog, data=repo)
+    for item in repo["tree"]:
+        if item["path"] == "content" and item["type"]=="tree":
+            url = "repos/"+"/".join(blog.repo.split("/")[:-1])+"/"+item["sha"]
+            tree = github.get(url+"?recursive=true", token=owner[0].github_token.to_token())
+            break
+    repo = tree.json()
 
-@login_required
-def getTree(blog_id, sha):
-    blog = db.session.execute(text("select * from blog b join blog_author ba on ba.blog_id=b.id where ba.author_id=:user_id and b.id =:id"), {"user_id": g.user.id, "id":blog_id}).fetchone()
-    owner=db.session.execute(select(Author).where(Author.id==blog.owner_id)).first()
-    url = "repos/"+"/".join(blog.repo.split("/")[:-1])+"/"+sha
-    tree = github.get(url, token=owner[0].github_token.to_token())
-    return tree.json()
+    # sort the tree 
+    trees = [None] * 10
+    for n in repo["tree"]:
+        n["path"]="content/"+n["path"]
+        n["name"]=n["path"].split("/")[-1]
+        if n["type"]=="tree":
+            n["tree"]=[]
+        path = n["path"].split("/")
+        if len(path)>0:
+            if trees[len(path)-1] == None:
+                trees[len(path)-1]=[]
+            trees[len(path)-1].append(n)
+
+    # create node tree
+    x = len(trees)-1
+    while x>0:
+        if trees[x] == None:
+            x-=1
+            continue
+        for y in trees[x]:
+            path = y["path"].split("/")
+            sub= "/".join(path[:-1])
+            if trees[x-1] == None:
+                continue
+            for z in trees[x-1]:
+                if z["path"] == sub:
+                    z["tree"].append(y)
+        x-=1
+    return render_template("blog/editor.html", blog=blog, data=trees[1])
 
 @login_required
 def getBlob(blog_id, sha):
--- routes/blog_bp.py
+++ routes/blog_bp.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from controllers.blog_controller import create, index, show, editor, delete, getTree, getBlob, saveBlob
+from controllers.blog_controller import create, index, show, editor, delete, getBlob, saveBlob
 
 blog_bp = Blueprint('blog_bp', __name__)
 
@@ -8,7 +8,6 @@ blog_bp = Blueprint('blog_bp', __name__)
 blog_bp.route('/', methods=['GET'])(index)
 blog_bp.route('/create', methods=['POST', "GET"])(create)
 blog_bp.route('/<int:blog_id>/editor')(editor)
-blog_bp.route('/<int:blog_id>/tree/<string:sha>')(getTree)
 blog_bp.route('/<int:blog_id>/blob/<string:sha>')(getBlob)
 blog_bp.route('/<int:blog_id>/blob/<string:sha>', methods=['POST'])(saveBlob)
 blog_bp.route('/<int:blog_id>', methods=['DELETE'])(delete)
